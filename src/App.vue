f1167369627ac3e8b8e137872a3a38b472e728e3
park.b.c <burn7@ezcaretech.com>
2023-06-28 09:03:15 +0900

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,66 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
+    <v-toolbar 
+      dark
+      prominent
+      density="comfortable">      
+      <v-app-bar-nav-icon/>
+      <v-toolbar-title>grey house</v-toolbar-title>
+      <v-spacer/>      
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+      <!-- <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>       -->    
+    </v-toolbar>  
+  
+    <v-layout>
+      <v-navigation-drawer                
+        permanent>
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home" title="grey house" :to="{name:home}">
+            <!-- <template v-slot:append>
+              <v-btn
+                variant="text"
+                icon="mdi-chevron-left"
+                @click.stop="rail = !rail">
+              </v-btn>
+            </template> -->
+          </v-list-item>
+
+          <v-list-item prepend-icon="md:event" title="calenda" />
+        </v-list>
+
+        <v-divider/>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles" :to="{name:about}"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <router-view/>
+      </v-main>
+    </v-layout>
   </v-app>
+  
 </template>
 
 <script>
+import { defineComponent } from 'vue';
 
-export default {
-  name: 'App',
+// Components
 
-  data: () => ({
-    //
-  }),
-}
+export default defineComponent({  
+});
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
 import HelloWorld from '../components/HelloWorld.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('../views/HomeView.vue')
-  },
+  { path: '/', name: 'home', component: () => Home },
+  { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
   // {
   //   path: '/dashboard',
   //   name: 'dashboard',
